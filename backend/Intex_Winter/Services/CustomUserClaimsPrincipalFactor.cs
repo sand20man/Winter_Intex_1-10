f14ab7b3bd3dbeab34c3320ef2e21c5a131dfb48
Jack Estes <jackestes@Jacks-MacBook-Pro-6.local>
2025-04-09 19:20:06 -0600

    Test pt5

--- backend/Intex_Winter/Services/CustomUserClaimsPrincipalFactor.cs
+++ backend/Intex_Winter/Services/CustomUserClaimsPrincipalFactor.cs
@@ -8,13 +8,19 @@ namespace Intex_Winter.Services;
 public class CustomUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<IdentityUser>
 {
     public CustomUserClaimsPrincipalFactory(
-        UserManager<IdentityUser> userManager, 
+        UserManager<IdentityUser> userManager,
+        RoleManager<IdentityRole> roleManager,
         IOptions<IdentityOptions> optionsAccessor)
         : base(userManager, optionsAccessor) { }
 
     protected override async Task<ClaimsIdentity> GenerateClaimsAsync(IdentityUser user)
     {
         var identity = await base.GenerateClaimsAsync(user);
+        var roles = await UserManager.GetRolesAsync(user);
+        foreach (var role in roles)
+        {
+            identity.AddClaim(new Claim(ClaimTypes.Role, role));
+        }
         identity.AddClaim(new Claim(ClaimTypes.Email, user.Email ?? "")); // Ensure email claim is always present
         return identity;
     }
